b3c9c363e884dcf91da41c84ae94ad9cd678e3c0
Build Mount <47172782+Build-Mount@users.noreply.github.com>
2024-05-05 19:52:27 +0900

    Add files via upload

---
+++ 01_transform-origin/transform-origin.js
@@ -0,0 +1,19 @@
+document.getElementById("transform-origin1").addEventListener("click", () => {
+  document.getElementById("transform-origin1").classList.add("rotate1");
+});
+
+document.getElementById("transform-origin2").addEventListener("click", () => {
+  document.getElementById("transform-origin2").classList.add("rotate2");
+});
+
+document.getElementById("transform-origin3").addEventListener("click", () => {
+  document.getElementById("transform-origin3").classList.add("rotate3");
+});
+
+document.getElementById("transform-origin4").addEventListener("click", () => {
+  document.getElementById("transform-origin4").classList.add("rotate4");
+});
+
+document.getElementById("transform-origin5").addEventListener("click", () => {
+  document.getElementById("transform-origin5").classList.add("rotate5");
+});
---
+++ 02_transform-origin2/transform-origin2.js
@@ -0,0 +1,19 @@
+document.getElementById("transform-origin1").addEventListener("click", () => {
+  document.getElementById("transform-origin1").classList.add("scale1");
+});
+
+document.getElementById("transform-origin2").addEventListener("click", () => {
+  document.getElementById("transform-origin2").classList.add("scale2");
+});
+
+document.getElementById("transform-origin3").addEventListener("click", () => {
+  document.getElementById("transform-origin3").classList.add("scale3");
+});
+
+document.getElementById("transform-origin4").addEventListener("click", () => {
+  document.getElementById("transform-origin4").classList.add("scale4");
+});
+
+document.getElementById("transform-origin5").addEventListener("click", () => {
+  document.getElementById("transform-origin5").classList.add("scale5");
+});
